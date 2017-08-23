35009c42683ed8a2a513050d11d817ebfbcde509
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2017-08-23 15:52:40 +0100

    includes google analytics screen tracking non-redux example

---
+++ google-analytics-screen-tracking.js
@@ -0,0 +1,33 @@
+import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
+
+const tracker = new GoogleAnalyticsTracker(GA_TRACKING_ID);
+
+// gets the current screen from navigation state
+function getCurrentRouteName(navigationState) {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getCurrentRouteName(route);
+  }
+  return route.routeName;
+}
+
+const AppNavigator = StackNavigator(AppRouteConfigs);
+
+export default () => (
+  <AppNavigator
+    onNavigationStateChange={(prevState, currentState) => {
+      const currentScreen = getCurrentRouteName(currentState);
+      const prevScreen = getCurrentRouteName(prevState);
+
+      if (prevScreen !== currentScreen) {
+        // the line below uses the Google Analytics tracker
+        // change the tracker here to use other Mobile analytics SDK.
+        tracker.trackScreenView(currentScreen);
+      }
+    }}
+  />
+);
